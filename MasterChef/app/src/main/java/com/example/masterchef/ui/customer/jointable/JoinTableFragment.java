d49dfd9897220fb2fee3658a4091688fda9050cc
quang <quangminhtran323@gmail.com>
2021-05-10 23:37:10 +0700

    edit menu

---
+++ MasterChef/app/src/main/java/com/example/masterchef/ui/customer/jointable/JoinTableFragment.java
@@ -0,0 +1,38 @@
+package com.example.masterchef.ui.customer.jointable;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.masterchef.R;
+
+public class JoinTableFragment extends Fragment {
+
+    public RecyclerView dataList;
+    int[] images = {R.drawable.table1, R.drawable.table2, R.drawable.table3, R.drawable.table1, R.drawable.table2};
+    String[] titles = {"Bàn 1", "Bàn 2", "Bàn 3", "Bàn 4", "Bàn 5"};
+    int[] seats = {4, 6, 8, 4, 6};
+    JoinTableAdapter adapter;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_customer_jointable, container, false);
+        dataList = root.findViewById(R.id.jointable_recyclerCategory);
+
+        adapter = new JoinTableAdapter(getActivity(), images, titles, seats);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3, GridLayoutManager.VERTICAL, false);
+        dataList.setLayoutManager(gridLayoutManager);
+        dataList.setAdapter(adapter);
+
+        return root;
+    }
+}
